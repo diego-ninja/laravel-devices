80cd3aeb8a8e4040ff5b520ab79c66bdd1c89f61
Diego Rin <yosoy@diego.ninja>
2024-10-31 22:58:13 +0100

    fix(helpers): fixed device_uuid function when retrieving uuid from request

--- helpers.php
+++ helpers.php
@@ -25,7 +25,7 @@ if (! function_exists('device_uuid')) {
     function device_uuid(): ?StorableId
     {
         $cookieName = Config::get('devices.device_id_cookie_name');
-        return Cookie::has($cookieName) ? DeviceIdFactory::from(Cookie::get($cookieName)) : request()->device_id;
+        return Cookie::has($cookieName) ? DeviceIdFactory::from(Cookie::get($cookieName)) : DeviceIdFactory::from(request()->device_id);
     }
 }
 
