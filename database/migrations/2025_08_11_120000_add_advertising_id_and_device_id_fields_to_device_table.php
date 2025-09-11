31905071e2a57c97cb8f42645a3ae7bcff7bfc28
Davide Pizzato <davide.pizzato@kimiagroup.com>
2025-09-11 16:37:26 +0200

    feat: improve index for advertising and device id

--- database/migrations/2025_08_11_120000_add_advertising_id_and_device_id_fields_to_device_table.php
+++ database/migrations/2025_08_11_120000_add_advertising_id_and_device_id_fields_to_device_table.php
@@ -9,14 +9,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('devices', function (Blueprint $table) {
-            $table->string('advertising_id')->nullable()->unique('idx_advertising_id_unique');
-            $table->string('device_id')->nullable()->unique('idx_device_id_unique');
+            $table->string('advertising_id')->nullable();
+            $table->string('device_id')->nullable();
+
+            $table->unique(['platform', 'advertising_id'], 'idx_platform_advertising_id_unique');
+            $table->unique(['platform', 'device_id'], 'idx_platform_device_id_unique');
         });
     }
 
     public function down(): void
     {
         Schema::table('devices', function (Blueprint $table) {
+            $table->dropIndex('idx_platform_advertising_id_unique');
+            $table->dropIndex('idx_platform_device_id_unique');
+
             $table->dropColumn('advertising_id');
             $table->dropColumn('device_id');
         });
